4f347e3c3439add0b6057d5757786ac103410804
Govind Sen <govindsen0915@gmail.com>
2023-09-01 08:05:20 +0530

    feat(): add alert controller

--- ToDoApp_Realm/ToDoApp_Realm/View/Controller/ViewController.swift
+++ ToDoApp_Realm/ToDoApp_Realm/View/Controller/ViewController.swift
@@ -12,11 +12,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var contactTableView: UITableView!
     
+    var contactArray = [Contact]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configuration()
     }
     @IBAction func addContactButtonTapped(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Add Contact", message: "Enter your contact details", preferredStyle: .alert)
+        let save = UIAlertAction(title: "Save", style: .default) { _ in
+            
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(save)
+        alertController.addAction(cancel)
+        present(alertController, animated: true)
     }
     
 
@@ -28,13 +39,13 @@ extension ViewController {
     }
 }
 
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-}
+//extension ViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//}
 
