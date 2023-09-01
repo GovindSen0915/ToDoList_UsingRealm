452a80fee7ec1f77eedb6b57ce0a3c4118a7dfa0
Govind Sen <govindsen0915@gmail.com>
2023-09-01 13:21:26 +0530

    feat(): add new database helper file and code

---
+++ ToDoApp_Realm/ToDoApp_Realm/Helper/DatabaseHelper.swift
@@ -0,0 +1,19 @@
+//
+//  DatabaseHelper.swift
+//  ToDoApp_Realm
+//
+//  Created by Govind Sen on 01/09/23.
+//
+
+import UIKit
+import RealmSwift
+
+class DatabaseHelper {
+    static let shared = DatabaseHelper()
+    
+    private var realm = try! Realm()
+    
+    func saveContact() {
+        
+    }
+}
